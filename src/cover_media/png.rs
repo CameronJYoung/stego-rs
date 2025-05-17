260274cd9a90f388b85177de6a20036442129e06
cameronjyoung <cameronjyoung123@gmail.com>
2025-05-17 11:13:27 +0100

    Implement cover media functionality

---
+++ src/cover_media/png.rs
@@ -0,0 +1,73 @@
+use image::RgbaImage;
+use crate::core::cover_media::StegoCoverMedia;
+use crate::core::error::StegoCoverMediaError;
+
+pub struct PngCoverMedia {
+    data: Vec<u8>,
+    height: u32,
+    width: u32
+}
+
+impl PngCoverMedia {
+    pub fn new(height: u32, width: u32) -> Self {
+        Self {
+            data: Vec::new(),
+            height,
+            width,
+        }
+    }
+
+    pub fn open(path: &str) -> Result<Self, StegoCoverMediaError> {
+        // Get the PNG using the image library
+        let image = match image::open(path) {
+            Ok(image) => image,
+            Err(e) => {
+                return Err(StegoCoverMediaError::BadFile(e.to_string()))
+            }
+        };
+
+        // Extract RGB data
+        let rgb_img = image.to_rgba8();
+
+        // Convert to a nicer type
+        let rgb_data_bytes: Vec<u8> = rgb_img.pixels().flat_map(|p| p.0).collect();
+
+        Ok(Self {
+            data: rgb_data_bytes,
+            height: image.height(),
+            width: image.width()
+        })
+    }
+
+    pub fn save(&self, output_path: &str) -> Result<(), StegoCoverMediaError> {
+        let new_image: RgbaImage = match RgbaImage::from_raw(self.width, self.height, self.data.clone()) {
+            Some(i) => i,
+            None => return Err(StegoCoverMediaError::GenerateFileFailure)
+        };
+
+        match new_image.save(output_path) {
+            Ok(_) => Ok(()),
+            Err(_) => return Err(StegoCoverMediaError::WriteFileFailure)
+        }
+    }
+}
+
+impl StegoCoverMedia for PngCoverMedia {
+    fn read_bytes(&self) -> &[u8] {
+        &self.data
+    }
+
+    fn write_bytes(&mut self, new_bytes: &[u8]) -> Result<(), StegoCoverMediaError> {
+        self.data.clear();
+        self.data.extend_from_slice(new_bytes);
+        Ok(())
+    }
+
+    fn clone_with_bytes(&self, new_bytes: &[u8]) -> Result<Box<dyn StegoCoverMedia>, StegoCoverMediaError> {
+        Ok(Box::new(Self {
+            data: new_bytes.to_vec(),
+            height: self.height,
+            width: self.width
+        }))
+    }
+}
---
+++ src/cover_media/wav_pcm.rs
@@ -0,0 +1,89 @@
+use hound::{WavReader, WavSpec, WavWriter};
+use crate::core::cover_media::StegoCoverMedia;
+use crate::core::error::StegoCoverMediaError;
+
+pub struct WavPcmCoverMedia {
+    data: Vec<u8>,
+    wav_spec: WavSpec
+}
+
+impl WavPcmCoverMedia {
+    pub fn new(wav_spec: WavSpec) -> Self {
+        Self {
+            data: Vec::new(),
+            wav_spec
+        }
+    }
+
+    pub fn open(path: &str) -> Result<Self, StegoCoverMediaError> {
+        // Get the wav reader using the hound crate
+        let mut wav_reader = match WavReader::open(path) {
+            Ok(w) => w,
+            Err(e) => return Err(StegoCoverMediaError::BadFile(e.to_string()))
+        };
+
+        // Get PCM data in bytes
+        let mut pcm_bytes: Vec<u8> = Vec::new();
+        for sample in wav_reader.samples::<i32>() { // TODO: This will fail if the supplied audio is not 32 bits. When we implement file type specific config we should make this configurable
+            let sample = match sample {
+                Ok(s) => s,
+                Err(e) => return Err(StegoCoverMediaError::BadFile(e.to_string()))
+            };
+
+            pcm_bytes.extend(&sample.to_le_bytes());
+        };
+
+        Ok(Self {
+            data: pcm_bytes,
+            wav_spec: wav_reader.spec()
+        })
+    }
+
+    pub fn save(&self, output_path: &str) -> Result<(), StegoCoverMediaError> {
+        // Create a hound writer
+        let mut new_wav_writer = match WavWriter::create(output_path, self.wav_spec) {
+            Ok(w) => w,
+            Err(_) => return Err(StegoCoverMediaError::WriteFileFailure)
+        };
+
+        // Chunk the final encoded data into samples and add it to the writer
+        for chunk in self.data.chunks_exact(4) {
+            let bytes: [u8; 4] = match chunk.try_into() {
+                Ok(t) => t,
+                Err(_) => return Err(StegoCoverMediaError::GenerateFileFailure)
+            };
+
+            let sample = i32::from_le_bytes(bytes);
+
+            if let Err(_) = new_wav_writer.write_sample(sample) {
+                return Err(StegoCoverMediaError::GenerateFileFailure);
+            }
+        }
+
+        // Write the file
+        if let Err(_) = new_wav_writer.finalize() {
+            return Err(StegoCoverMediaError::GenerateFileFailure);
+        }
+
+        Ok(())
+    }
+}
+
+impl StegoCoverMedia for WavPcmCoverMedia {
+    fn read_bytes(&self) -> &[u8] {
+        &self.data
+    }
+
+    fn write_bytes(&mut self, new_bytes: &[u8]) -> Result<(), StegoCoverMediaError> {
+        self.data.clear();
+        self.data.extend_from_slice(new_bytes);
+        Ok(())
+    }
+
+    fn clone_with_bytes(&self, new_bytes: &[u8]) -> Result<Box<dyn StegoCoverMedia>, StegoCoverMediaError> {
+        Ok(Box::new(Self {
+            data: new_bytes.to_vec(),
+            wav_spec: self.wav_spec
+        }))
+    }
+}
